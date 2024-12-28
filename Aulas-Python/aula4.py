513b7c0e136a22e6723de0b862db257886727a51
Allan Richard Bandeira <allanrichard1345@gmail.com>
2024-12-28 01:24:52 -0300

    Tipo de dado boleano(bool)

--- Aulas-Python/aula4.py
+++ Aulas-Python/aula4.py
@@ -1,8 +1,12 @@
 # Tipo de dado bool (boolean)
-# Ao questionar algo rm um programa,
+# Ao questionar algo em um programa,
 # Só existem duas respostas possíveis:
 # sim (True) ou não (False).
 # Existem vários operadores para "questionar".
 # Dentre eles o ==, que é um operador lógico que questiona se um valor e igual a outro
 print(10 == 10) # Sim => True (verdadeiro)
-print(10 == 11) # Não => False (falso)
+print(10 == 11) # Não => False (falso) 
+print(type(True))
+print(type(False))
+print(type(10 == 10))
+print(type(10 == 11))
