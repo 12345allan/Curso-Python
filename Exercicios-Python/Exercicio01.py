31459c4ff83163fa378d2acda9aa7eff245d01a6
Allan Richard Bandeira <allanrichard1345@gmail.com>
2024-12-29 11:34:15 -0300

    Arquivo movido para uma pasta exclusiva

---
+++ Exercicios-Python/Exercicio01.py
@@ -0,0 +1,22 @@
+# Atividade 01: Criar as variáveis com os tipos de dados que foram dados nas aulas do curso 
+# Minhas resostas:
+nome = 'Allan'
+print('Nome:', nome)
+sobrenome = 'Richard'
+print('Sobrenome:', sobrenome)
+idade = 23
+print('Idade:', idade)
+ano_nascimento = 2001
+print('Ano de nascimento:', ano_nascimento)
+maior_de_idade = idade >= 18
+print('É maior de idade?', maior_de_idade)
+altura_metros = 1.70
+print('Altura em metros:', altura_metros)
+
+# # Resolução da Aividade feita pelo professor:
+# nome = 'Allan'
+# sobrenome = 'Richard'
+# idade = 23
+# ano_nascimento = 2024 - idade
+# maior_de_idade = idade >= 18
+# altura_metros = 1.70
