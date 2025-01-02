a8827217b2bef8fd85b29c50cfe2dd316b09aa19
Allan Richard Bandeira <allanrichard1345@gmail.com>
2025-01-02 10:45:38 -0300

    Conhecendo os operadores aritmetricos

---
+++ Aulas-Python/aula7.py
@@ -0,0 +1,22 @@
+# Operadores Aritmetricos
+
+adicao = 10 + 10 
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2 # Aqui sempre retornará um número de ponto flutuante (Float)
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2 # Ela corta as casas decimais
+print('Divisão Inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponeciação', exponenciacao)
+
+modulo = 25 % 5 # Resto da divisão 
+print('Modulo', modulo)
