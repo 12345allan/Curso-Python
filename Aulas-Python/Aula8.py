5061113d07aa04bf36f1d02980c741ad3f527bac
Allan Richard Bandeira <allanrichard1345@gmail.com>
2025-01-02 11:21:39 -0300

    Entendendo o conceito de concatenação e repetição

---
+++ Aulas-Python/Aula8.py
@@ -0,0 +1,10 @@
+# Concatenação (+) e repetição (*) com operadores aritmetricos
+# concatenação é juntar os valores usando o sinal de + e usar strings para fazer a cocatenação 
+contatenacao = 'A' + 'B' + 'C'
+print(contatenacao)
+
+# Repetição usando o operador de multiplicação com um número inteiro e uma string qualquer
+a_dez_vezes = 'A' * 10
+tres_vezes_luiz = 3 * 'luiz'
+print(a_dez_vezes)
+print(tres_vezes_luiz)
