5943eed4fd194822fbec98932f2facebd353435d
Allan Richard Bandeira <allanrichard1345@gmail.com>
2025-01-07 00:57:04 -0300

    Resolução da Atividade 2 de IMC, com resolução do professor

---
+++ Exercicios-Python/Exercicio02.py
@@ -0,0 +1,23 @@
+# Exercicio de Calculo de Massa corpórea (IMC)
+
+nome = 'Allan Richard'
+altura = 1.80
+peso = 95
+imc = peso / (altura ** 2)
+print('Senhor' + (' '), nome, 'pesa' + (' '), str(peso), 'e seu' + (' '), 'é :' , str(imc))
+"""
+Dados Para a Atividade:
+Allan Richard Bandeira,
+pesa 95 quilos e seu IMC é
+29.320987654320987
+"""
+
+
+# Resolução feita pelo o professor: 
+#nome = 'Allan Richard'
+#altura = 1.80
+#peso = 95
+#imc = peso / altura ** 2
+#print(nome, 'tem', altura, 'de altura', ',')
+#print('pesa', peso, 'quilos e seu imc é',)
+#print(imc)
