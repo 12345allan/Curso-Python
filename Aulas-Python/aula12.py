4749ce3e72b3269ee8800f852f237b7f8c1d309d
Allan Richard Bandeira <allanrichard1345@gmail.com>
2025-01-18 02:45:49 -0300

    Conhecendo os laços de repetição

---
+++ Aulas-Python/aula12.py
@@ -0,0 +1,13 @@
+# Laços de repetição muda o fluxo do código
+# elif e else depende do if
+# O if não depende de ninguem 
+# if  / elif      / else
+# se / se não se / se não
+entrada = input('Voce quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar': 
+    print('Voce entrou no sistema')
+elif entrada == 'sair':
+    print('Voce saiu do sistema')
+else:
+    print('Voce não digitou nem entrar e nem sair')
