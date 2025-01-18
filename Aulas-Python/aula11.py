3cdcc7af4fd213c44c688a130a0f830f4dc05446
Allan Richard Bandeira <allanrichard1345@gmail.com>
2025-01-18 02:07:32 -0300

    Função input conhecendo ela e entendendo como funciona

---
+++ Aulas-Python/aula11.py
@@ -0,0 +1,11 @@
+# input coleta dados de úsuarios ou informções e o input sempre retornará uma string na saida do programa 
+
+
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+
+# Criando duas novas variaveis para fazer a conversão de uma string para um inteiro  
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'A soma dos números é: {int_numero_1 + int_numero_2}')
