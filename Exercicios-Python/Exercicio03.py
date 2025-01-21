da1c40373c2fdd9731f7dd0e02959f1d665c5aa5
Allan Richard Bandeira <allanrichard1345@gmail.com>
2025-01-21 12:12:45 -0300

    Atividade Resolvida  por mim e embaixo a resolução do professor

---
+++ Exercicios-Python/Exercicio03.py
@@ -0,0 +1,26 @@
+# Exercicio de Programação com if e comparação
+"""Feito por mim"""
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite um segundo valor: ')
+
+if primeiro_valor > segundo_valor:
+    print(f'primeiro_valor= {primeiro_valor} é maior que o segundo_valor={segundo_valor}')
+elif primeiro_valor < segundo_valor:
+    print(f'segundo_valor= {primeiro_valor} é menor que o primeiro_valor= {segundo_valor}') 
+
+
+"""Feito pelo professor:
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite um segundo valor: ')
+
+if primeiro_valor > segundo_valor:
+    print(
+    f'{primeiro_valor=} é maior'
+    f' do que {segundo_valor}'
+    )
+else: 
+    print(
+    f'{segundo_valor=} é maior'
+    f'do que {primeiro_valor=}'
+    ) 
+"""
